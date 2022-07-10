66511322adfa9179a510e736d4663ea0f4c282f8
ryowuandjanet <ryowu0329@gmail.com>
2022-07-10 11:01:17 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <h1>This is the INSTAGRAM 2.0 BUILD</h1>
+    </div>
+  )
+}
