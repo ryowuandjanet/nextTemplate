a08ece7020240036e4bb902a95c0720c467200a8
ryowuandjanet <ryowu0329@gmail.com>
2022-07-10 11:08:54 +0000

    add tailwindcss package

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>是吳小毛</title>
       </Head>
-      <h1>This is the INSTAGRAM 2.0 BUILD</h1>
+      <h1 className="text-red-700">真的可以即時修改</h1>
     </div>
   )
 }
